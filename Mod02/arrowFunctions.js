0349ad01e598fd8e4e6e1fb6c28640d1257282b0
hack-panda <dthesec@gmail.com>
2022-10-09 14:59:25 +0530

    Javascript More of Arrow Functions

--- Mod02/arrowFunctions.js
+++ Mod02/arrowFunctions.js
@@ -6,3 +6,8 @@ printMyName("Alok");
 
 const square = (num) => num * num;
 console.log(square(23));
+
+const number = [1, 2, 3, 4];
+const doubleofNumber = number.map((num) => num * 2);
+console.log(number);
+console.log(doubleofNumber);
