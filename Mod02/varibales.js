a68aebf9ad8792bdb080d4a29c9ad859a7b1b5a5
hack-panda <dthesec@gmail.com>
2022-10-09 11:50:16 +0530

    Learnt about let and const

---
+++ Mod02/varibales.js
@@ -0,0 +1,10 @@
+const Name="Alok" //constant values
+let age=25 //varible values
+console.log(Name);
+console.log(age);
+
+age=27;
+console.log(age)
+
+Name="Abhay" //Will throw error
+console.log(Name)
