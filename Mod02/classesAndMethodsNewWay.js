d70187f3a2a5966d90083adc23cdb884dc2ee86c
hack-panda <dthesec@gmail.com>
2022-10-09 14:36:19 +0530

    Learnt about Javascript Classes and Objects New Method

---
+++ Mod02/classesAndMethodsNewWay.js
@@ -0,0 +1,18 @@
+class Human {
+  gender = "Male";
+
+  printGender = () => {
+    console.log(this.gender);
+  };
+}
+
+class Person extends Human {
+  name = "Anand";
+  printMyName = () => {
+    console.log(this.name);
+  };
+}
+
+const person = new Person();
+person.printMyName();
+person.printGender();
